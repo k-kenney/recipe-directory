abf2b45a1bd7bcb21af5a20bc2fa7a0c0f784dbc
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-08-08 10:29:50 +0300

    barebones of recipe directory

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+// page components
+import Navbar from './components/Navbar'
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Search from './pages/search/Search'
+import Recipe from './pages/recipe/Recipe'
+
+//  styles
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/recipes/:id" element={<Recipe />} />
+          <Route path="/redirect" element={<Navigate to="/home" />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App
---
+++ src/pages/recipe/Recipe.jsx
@@ -0,0 +1,27 @@
+import { useParams } from "react-router-dom"
+import { useFetch } from "../../hooks/useFetch"
+
+import './Recipe.css'
+
+export default function Recipe() {
+  const { id } = useParams()
+  const url = 'http://localhost:3000/recipes/' + id
+  const { data: recipe, isPending, error } = useFetch(url)
+
+  return (
+    <div className="recipe">
+      {error && <p className="error">{error}</p>}
+      {isPending && <div>Loading...</div>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+    )}
+    </div>
+  )
+}
